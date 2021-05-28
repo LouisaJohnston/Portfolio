6397d724713b03cd33eee154682b37bad8be422b
LouisaJohnston <louisa.latham.johnston@gmail.com>
2021-05-28 11:26:32 -0700

    begin nav styling

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -4,24 +4,26 @@ import Link from 'next/link'
 export default function Navbar() {
     return (
         <nav>
-            <Link href='#about-container'>
-                <a>About Me</a>
-            </Link>
-            <Link href='#projects-container'>
-                <a>Projects</a>
-            </Link>
-            <Link href='#languages-container'>
-                <a>Languages/Frameworks</a>
-            </Link>
-            <Link href='https://github.com/LouisaJohnston'>
-                <a target='_blank'>Github</a>
-            </Link>
-            <Link href='https://www.linkedin.com/in/louisa-johnston/'>
-                <a target='_blank'>LinkedIn</a>
-            </Link>
-            <Link href='/LouisaJohnston_Resume.pdf'>
-                <a target='_blank' download>Download Resume</a>
-            </Link>
+            <div id='nav-links'>
+                <Link href='#about'>
+                    <a>About Me</a>
+                </Link>
+                <Link href='#projects'>
+                    <a>Projects</a>
+                </Link>
+                <Link href='#languages'>
+                    <a>Languages/Frameworks</a>
+                </Link>
+                <Link href='https://github.com/LouisaJohnston'>
+                    <a target='_blank'>Github</a>
+                </Link>
+                <Link href='https://www.linkedin.com/in/louisa-johnston/'>
+                    <a target='_blank'>LinkedIn</a>
+                </Link>
+                <Link href='/LouisaJohnston_Resume.pdf'>
+                    <a target='_blank' download>Download Resume</a>
+                </Link>
+            </div>
         </nav>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -8,74 +8,76 @@ export default function Home() {
         <title>Louisa Johnston</title>
         <link rel="icon" href="/portfolio_favicon_squareheavy.png" />
       </Head>
-      <h1>Louisa Johnston</h1>
+      <div id="main">
+        <h1>Louisa Johnston</h1>
 
-      <div id="about-container">
-        <div>
-          <h2>Greetings!</h2>
-          <p>I am a full-stack engineer specializing in front-end 
-          development. My particular interest is in ventures 
-          with an orientation towards social equity and the arts.</p>   
+        <div id="about">
+          <div>
+            <h2>Greetings!</h2>
+            <p>I am a full-stack engineer specializing in front-end 
+            development. My particular interest is in ventures 
+            with an orientation towards social equity and the arts.</p>   
+          </div>
+          <div>
+            <p>I am a huge fan of breaking problems down into their 
+            smallest parts in order to build successful interventions. 
+            My background in nonprofit marketing has taught me the 
+            value of metrics-driven work, creative workarounds, and 
+            constructing projects from the user’s perspective.</p>
+          </div>
+          <div>
+            <p>In my spare time, I enjoy caring for my brood of houseplants, 
+            an inordinate amount of crossword puzzles, and inundating friends
+            and family with my excess baked goods.</p>
+          </div>
         </div>
-        <div>
-          <p>I am a huge fan of breaking problems down into their 
-          smallest parts in order to build successful interventions. 
-          My background in nonprofit marketing has taught me the 
-          value of metrics-driven work, creative workarounds, and 
-          constructing projects from the user’s perspective.</p>
+
+
+        <div id="projects">
+          <h2>Projects</h2>
+          
+          <a><h3>Bot Noggle</h3></a>                                                                                                                                                              
+          <h4>Python, Pygame</h4>
+          <ul>
+            <li>Built a version of Boggle using Python and Pygame to render a 4x4 grid out of a two-dimensional array of randomized letters corresponding to real Boggle dice</li>
+            <li>Allowed users to check whether a given word exists in adjacent grid tiles (up, down, diagonally, forwards, and backwards)</li>
+            <li>Inserted dictionary information into a trie data structure to check against user input and optimize the project's time complexity</li>
+          </ul>
+          <a href="https://github.com/LouisaJohnston/project4" target="_blank"><p>Project Repo</p></a>
+
+          <h3>Paw-Tel (Group Project)</h3>
+          <h4>ReactJS, Mongoose, MongoDB, and MERN auth</h4>
+          <ul>
+            <li>Designed a full-stack MERN app using complex authorization routes allowing users to register as either pet owner or as a pet hotel owner to help pet owners connect with pet hotels</li>
+            <li>Employed React components to render information from a local API pertaining to user-specific pets or hotels, depending on the authorization route, with the ability to update and delete data fields</li>
+            <li>Saved user input through server-side Mongoose database using MongoDB</li>
+          </ul>
+          <a href="https://github.com/LouisaJohnston/Paw-tel" target="_blank"><p>Project Repo (Server-Side)</p></a>
+          <a href="https://github.com/LouisaJohnston/pawtel-client" target="_blank"><p>Project Repo (Client-Side)</p></a>
+
+          <h3>Flick Picks</h3>
+          <h4>EJS, ExpressJS, Axios, NodeJS, and PostgreSQL</h4>
+          <ul>
+            <li>Created a web app using NodeJS and ExpressJS with the ability for users to create an account or login with encrypted cookie storage and password hashing</li>
+            <li>Applied EJS templates and Axios to render information from a third-party film API and allow users to save movies to personal lists</li>
+            <li>Stored multiple user inputs (login information and movie comments/reviews) as database information fields through PostgreSQL with the ability for users to update them directly</li>
+          </ul>
+          <a href="https://github.com/LouisaJohnston/project2" target="_blank"><p>Project Repo</p></a>
+
+          <h3>Word-cross’d Puzzler</h3>
+          <h4>JavaScript, CSS3, and HTML5</h4>
+          <ul>
+            <li>Designed a grid-based 5x5 puzzle to run on in-browser for desktop computers</li>
+            <li>Utilized CSS and HTML properties to approximate the experience of a pen-and-paper puzzle with front-end styling considerations implemented through Flexbox</li>
+            <li>Developed functionality through JavaScript array manipulation to show time elapsed, toggle between vertical and horizontal input focus flow, check answers, reveal the puzzle, and restart the game through DOM event listeners</li>
+          </ul>
+          <a href="https://github.com/LouisaJohnston/Word-crossd_Puzzler" target="_blank"><p>Project Repo</p></a>
         </div>
-        <div>
-          <p>In my spare time, I enjoy caring for my brood of houseplants, 
-          an inordinate amount of crossword puzzles, and inundating friends
-          and family with my excess baked goods.</p>
+
+        <div id="languages">
+          <h2>Languages/Frameworks/Misc. Know-How</h2>
+          <p>Python, ReactJS, Mongoose, MongoDB, ExpressJS, NodeJS, SQL, PostgreSQL, EJS, Axios, JavaScript, CSS3, HTML5, Heroku, GitHub, Pygame, Adobe Photoshop, Adobe InDesign, Adobe Premiere, WordPress, Squarespace, NationBuilder, Google Analytics, Cargo, and Canva</p>
         </div>
-      </div>
-
-
-      <div id="projects-container">
-        <h2>Projects</h2>
-        
-        <a><h3>Bot Noggle</h3></a>                                                                                                                                                              
-        <h4>Python, Pygame</h4>
-        <ul>
-          <li>Built a version of Boggle using Python and Pygame to render a 4x4 grid out of a two-dimensional array of randomized letters corresponding to real Boggle dice</li>
-          <li>Allowed users to check whether a given word exists in adjacent grid tiles (up, down, diagonally, forwards, and backwards)</li>
-          <li>Inserted dictionary information into a trie data structure to check against user input and optimize the project's time complexity</li>
-        </ul>
-        <a href="https://github.com/LouisaJohnston/project4" target="_blank"><p>Project Repo</p></a>
-
-        <h3>Paw-Tel (Group Project)</h3>
-        <h4>ReactJS, Mongoose, MongoDB, and MERN auth</h4>
-        <ul>
-          <li>Designed a full-stack MERN app using complex authorization routes allowing users to register as either pet owner or as a pet hotel owner to help pet owners connect with pet hotels</li>
-          <li>Employed React components to render information from a local API pertaining to user-specific pets or hotels, depending on the authorization route, with the ability to update and delete data fields</li>
-          <li>Saved user input through server-side Mongoose database using MongoDB</li>
-        </ul>
-        <a href="https://github.com/LouisaJohnston/Paw-tel" target="_blank"><p>Project Repo (Server-Side)</p></a>
-        <a href="https://github.com/LouisaJohnston/pawtel-client" target="_blank"><p>Project Repo (Client-Side)</p></a>
-
-        <h3>Flick Picks</h3>
-        <h4>EJS, ExpressJS, Axios, NodeJS, and PostgreSQL</h4>
-        <ul>
-          <li>Created a web app using NodeJS and ExpressJS with the ability for users to create an account or login with encrypted cookie storage and password hashing</li>
-          <li>Applied EJS templates and Axios to render information from a third-party film API and allow users to save movies to personal lists</li>
-          <li>Stored multiple user inputs (login information and movie comments/reviews) as database information fields through PostgreSQL with the ability for users to update them directly</li>
-        </ul>
-        <a href="https://github.com/LouisaJohnston/project2" target="_blank"><p>Project Repo</p></a>
-
-        <h3>Word-cross’d Puzzler</h3>
-        <h4>JavaScript, CSS3, and HTML5</h4>
-        <ul>
-          <li>Designed a grid-based 5x5 puzzle to run on in-browser for desktop computers</li>
-          <li>Utilized CSS and HTML properties to approximate the experience of a pen-and-paper puzzle with front-end styling considerations implemented through Flexbox</li>
-          <li>Developed functionality through JavaScript array manipulation to show time elapsed, toggle between vertical and horizontal input focus flow, check answers, reveal the puzzle, and restart the game through DOM event listeners</li>
-        </ul>
-        <a href="https://github.com/LouisaJohnston/Word-crossd_Puzzler" target="_blank"><p>Project Repo</p></a>
-      </div>
-
-      <div id="languages-container">
-        <h2>Languages/Frameworks/Misc. Know-How</h2>
-        <p>Python, ReactJS, Mongoose, MongoDB, ExpressJS, NodeJS, SQL, PostgreSQL, EJS, Axios, JavaScript, CSS3, HTML5, Heroku, GitHub, Pygame, Adobe Photoshop, Adobe InDesign, Adobe Premiere, WordPress, Squarespace, NationBuilder, Google Analytics, Cargo, and Canva</p>
       </div>
     </div>
   )
