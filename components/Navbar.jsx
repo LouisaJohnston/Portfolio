24d7fd1284d537a9f4c24bc733d519fcebfcad03
LouisaJohnston <louisa.latham.johnston@gmail.com>
2021-05-28 11:38:58 -0700

    more nav styling

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -8,18 +8,23 @@ export default function Navbar() {
                 <Link href='#about'>
                     <a>About Me</a>
                 </Link>
-                <Link href='#projects'>
-                    <a>Projects</a>
-                </Link>
+
                 <Link href='#languages'>
                     <a>Languages/Frameworks</a>
                 </Link>
+
+                <Link href='#projects'>
+                    <a>Projects</a>
+                </Link>
+
                 <Link href='https://github.com/LouisaJohnston'>
                     <a target='_blank'>Github</a>
                 </Link>
+
                 <Link href='https://www.linkedin.com/in/louisa-johnston/'>
                     <a target='_blank'>LinkedIn</a>
                 </Link>
+                
                 <Link href='/LouisaJohnston_Resume.pdf'>
                     <a target='_blank' download>Download Resume</a>
                 </Link>
