2c5089e64d37267eaa30528356a2a7d13fddeb2a
LouisaJohnston <louisa.latham.johnston@gmail.com>
2021-06-29 17:25:38 -0700

    move navbar

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import '../styles/globals.css'
-import Navbar from '../components/Navbar/Navbar'
-import Footer from '../components/Footer/Footer'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
 import { MemoryRouter } from 'react-router-dom'
 
 export default function App({ Component, pageProps }) {
