e7460875ddd025a965d26efd52801cd471374185
LouisaJohnston <louisa.latham.johnston@gmail.com>
2021-09-15 23:17:32 -0700

    working dynamic graph

--- components/Graph.jsx
+++ components/Graph.jsx
@@ -1,34 +1,41 @@
-import Line from "./Line"
-import BarTextContent from "./BarTextContent"
-import Bar from "./Bar"
+import Line from "./Line";
+import BarTextContent from "./BarTextContent";
+import Bar from "./Bar";
 
-export default function Graph({mostUsed}) {
-    console.log(mostUsed)
-    const renderLines = () => {
-        return Array(10).fill(null).map((tech, i) => (
-            <Line 
-                left={i * 10}
-                key={i}
+export default function Graph({ mostUsed }) {
+  console.log(mostUsed);
+  const renderLines = () => {
+    return Array(10)
+      .fill(null)
+      .map((tech, i) => <Line left={i * 11.1} key={i} />);
+  };
+
+  const renderBars = () => {
+      return mostUsed.map((tech) => {
+        const percent = (tech.count / mostUsed[0].count) * 100;
+        return (
+            <Bar 
+                key={tech.tech}
+                percent={percent}
             />
-        ))
+        )
+      });
     }
+  };
 
-    const renderBars = () => {
-        let techSum = 
-    }
+  return (
+    <div className="graph-wrapper">
+      <div className="graph">
+        <BarTextContent />
+        <div className="bar-lines-container">
+          {renderLines()}
+          {renderBars()}
 
-    return (
-        <div className="graph-wrapper">
-            <div className="graph">
-                <BarTextContent />
-                <div className="bar-lines-container">
-                    {renderLines()}
-
-                    <Bar percent={50} />
-                    <Bar percent={25} />
-                    <Bar percent={25} />
-                </div>
-            </div>
+          {/* <Bar percent={50} />
+          <Bar percent={25} />
+          <Bar percent={25} /> */}
         </div>
-    )
+      </div>
+    </div>
+  );
 }
