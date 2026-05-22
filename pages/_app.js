35421820dea17221ed62347cb3c5e3f6d252453f
LouisaJohnston <louisa.latham.johnston@gmail.com>
2026-05-22 11:32:15 -0700

    update next js

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,13 @@
 import '../styles/globals.css'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
-import { MemoryRouter } from 'react-router-dom'
 
 export default function App({ Component, pageProps }) {
   return (
-    <MemoryRouter>
+    <>
       <Navbar />
       <Component {...pageProps} />
       <Footer />
-    </MemoryRouter>
-  ) 
+    </>
+  )
 }
