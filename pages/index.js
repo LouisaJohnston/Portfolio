2bd48fff0b0f2fc12d1fec885028790f3e8b7a3e
LouisaJohnston <louisa.latham.johnston@gmail.com>
2026-08-04 13:49:10 -0700

    fix typo

--- pages/index.js
+++ pages/index.js
@@ -52,7 +52,7 @@ export default function Index() {
           />
           <IntroParagraph
             paragraph={
-              "In my spare time, I can be found using the phone-a-friend approach to crossword puzzles or saying “hi!” to neighboorhood dogs."
+              "In my spare time, I can be found using the phone-a-friend approach to crossword puzzles or saying “hi!” to neighborhood dogs."
             }
           />
         </div>
